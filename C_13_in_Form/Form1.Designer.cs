d859a8d0bf743a9a26880b571931a5462e4a6795
Cat-Ok <r368322@gmail.com>
2015-03-07 21:27:06 +0200

    new_commit

--- C_13_in_Form/Form1.Designer.cs
+++ C_13_in_Form/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,7 +42,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Previous";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -61,7 +62,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Next";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
@@ -74,11 +75,22 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(245, 278);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Load Foto";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1342, 701);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -96,6 +108,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- C_13_in_Form/Form1.cs
+++ C_13_in_Form/Form1.cs
@@ -14,7 +14,7 @@ namespace C_13_in_Form
     {
         public static int image_count = 0;
         Graphics gr;
-        Image[] img = new Image[5];
+        Image[] img = new Image[7];
         public Form1()
         {
             InitializeComponent();
@@ -24,16 +24,34 @@ namespace C_13_in_Form
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                img[i] = Image.FromFile("D:\\Франка\\2-й курс ПМа - 22\\2-й семестр\\Обчислювальна Парктика\\C_13\\C_13_in_Form\\Астон\\"+i+".jpg");
-            }
-            gr = this.CreateGraphics();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Image = Image.FromFile("D:\\Франка\\2-й курс ПМа - 22\\2-й семестр\\Обчислювальна Парктика\\C_13\\C_13_in_Form\\suits_charactergallery_harvey_11.JPG");
+            //pictureBox1.Image = Image.FromFile("D:\\Франка\\2-й курс ПМа - 22\\2-й семестр\\Обчислювальна Парктика\\C_13\\C_13_in_Form\\suits_charactergallery_harvey_11.JPG");
+            if ((image_count--) <= 0)
+            {
+                image_count = 0;
+                gr = this.CreateGraphics();
+                gr.DrawImage(img[image_count], new Point(0, 0));
+                gr.Dispose();
+            }
+            else
+            {
+                if (image_count >= 0)
+                {
+                    gr = this.CreateGraphics();
+                    gr.DrawImage(img[image_count], new Point(0, 0));
+                    gr.Dispose();
+                }
+                else
+                {
+                    MessageBox.Show("Click Next!");
+                }
+            }
+            
+          
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -43,11 +61,38 @@ namespace C_13_in_Form
 
         private void button3_Click(object sender, EventArgs e)
         {
-            image_count++;
+            if ((image_count++) >= 6)
+            {
+                MessageBox.Show("Click Previous!");
+                image_count = 6;
+                gr = this.CreateGraphics();
+                gr.DrawImage(img[image_count], new Point(0, 0));
+                gr.Dispose();
+            }
+            else
+            {
+                if (image_count < 7)
+                {
+                    gr = this.CreateGraphics();
+                    gr.DrawImage(img[image_count], new Point(0, 0));
+                    gr.Dispose();
+                }
+                else
+                {
+                    MessageBox.Show("Click Previous!");
+                }
+            }
+          
             
-            int y = 5;
-            gr.DrawImage(img[image_count], new Point(0,0));
-            gr.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                img[i] = Image.FromFile("D:\\Франка\\2-й курс ПМа - 22\\2-й семестр\\Обчислювальна Парктика\\C_13\\C_13_in_Form\\Астон\\" + i + ".jpg");
+            }
+           
         }
     }
 }
